51d7bee4bcb20e74187128decc58fd2295c28ac1
Gaurav Bansode <gauravbansode75@gmail.com>
2025-12-15 20:56:42 +0530

    config: make baseURL environment-driven

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
     // public string $baseURL = 'http://localhost/mywebsite/public/';
-public string $baseURL = 'https://king-prawn-app-ca4ji.ondigitalocean.app/';
+public string $baseURL = '';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
