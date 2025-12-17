ecc7f5e52e8b8911f933fa01160cbb4aa3c5d7b0
Gaurav Bansode <gauravbansode75@gmail.com>
2025-12-17 21:45:31 +0530

    Fix production indexPage for DigitalOcean App Platform

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
     // public string $baseURL = 'http://localhost/mywebsite/public/';
-public string $baseURL = 'https://principacore.com/';
+public string $baseURL = '';
 
 public bool $maintenance = false;
 
